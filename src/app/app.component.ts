030dc6002d366cc32c6b02f50063375768324538
Joze Pesich <joze.pesich@health.telstra.com>
2017-07-27 14:55:53 +0800

    leaflet demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+
+import { map } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit {
+  mapDiv: HTMLElement;
+  myMap: L.Map;
+
+  constructor(private elRef: ElementRef) {
+  }
+  ngAfterViewInit() {
+    this.mapDiv = this.elRef.nativeElement.querySelector('.map');
+    console.log('this.mapDiv: ', this.mapDiv);
+
+    this.myMap = map(this.mapDiv).setView([51.505, -0.09], 13);
+  }
 }
